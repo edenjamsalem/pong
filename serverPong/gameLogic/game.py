44a0ffcb0b9857c2f7291c3385a38141c11c8f60
edenbeden98 <edenbeden98@gmail.com>
2025-06-23 04:08:52 +0100

    removing gamestate

--- serverPong/gameLogic/game.py
+++ serverPong/gameLogic/game.py
@@ -19,36 +19,22 @@ class Game:
         #     self.player2 = AIBot()
         self.ball = Ball((self.player1, self.player2), self._update_score)
 
-        # game state
-        self.state = {
-            "player1": {'y': self.player1.rect.y, 'dy': 0},
-            "player2": {'y': self.player2.rect.y, 'dy': 0},
-            "ball": {'x': self.ball.rect.x, 'y': self.ball.rect.y},
-            "score": {'player1': 0, 'player2': 0}
-        }
+        self.score = {'player1': 0, 'player2': 0}
 
     # private methods
     def _update_score(self, player):
-        self.state['score'][player] += 1
-
-    def _update_state(self):
-        self.state['player1']['y'] = self.player1.rect.y
-        self.state['player2']['y'] = self.player2.rect.y
-        self.state['ball']['x'] = self.ball.rect.x
-        self.state['ball']['y'] = self.ball.rect.y
-        # need to send a 'dy = 0' API request on player's keyup !
+        pass
 
     def _process_queue(self):
         while not self.queue.empty():
             player, movement = self.queue.get_nowait()
-            self.state[player]['dy'] = movement
 
     # public methods
     def queue_movement(self, player, dy):
         self.queue.put((player, dy))
 
     def get_state(self):
-        return (self.state.copy())
+        return ()
 
     def run(self):
         while self.running:
